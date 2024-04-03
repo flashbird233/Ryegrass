dfb74c7fc7a91eecf0a2bdc753ebfd37cc03f0d2
flashbird233 <moqianchen9811@gmail.com>
2024-04-03 23:07:37 +1100

    Changed the pyrgrass table structure

--- Breathe_Ease_Home/models.py
+++ Breathe_Ease_Home/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -28,11 +29,30 @@ class Pyegrass(models.Model):
     # id
     pye_id = models.IntegerField(primary_key=True, auto_created=True, null=False)
     # latitude
-    pye_lat = models.FloatField(max_length=9, default=None)
+    pye_lat = models.FloatField(max_length=10, default=None)
     # longitude
-    pye_lon = models.FloatField(max_length=9, default=None)
+    pye_lon = models.FloatField(max_length=10, default=None)
     # datetime
-    pye_datetime = models.DateTimeField(max_length=50, default=None)
+    pye_datetime = models.DateTimeField(max_length=50, default=None, null=True)
+    # year
+    pye_year = models.IntegerField(default=None, null=True,
+                                   validators=[MinValueValidator(1000), MaxValueValidator(2021)])
+    # month
+    pye_month = models.IntegerField(default=None, null=True,
+                                    validators=[MinValueValidator(1), MaxValueValidator(12)])
+    # day
+    pye_day = models.IntegerField(default=None, null=True,
+                                  validators=[MinValueValidator(1), MaxValueValidator(31)])
+    # rye_record_by
+    pye_record_by = models.CharField(max_length=200, default=None, null=True)
+    # rye_country
+    pye_country = models.CharField(max_length=50, default=None, null=True)
+    # rye_country_code
+    pye_country_code = models.CharField(max_length=10, default=None, null=True)
+    # rye_scientific_name
+    pye_scientific_name = models.CharField(max_length=100, default=None, null=True)
+    # rye_vernacular_name
+    pye_vernacular_name = models.CharField(max_length=100, default=None, null=True)
 
     # return pyegrass record
     def __str__(self):
