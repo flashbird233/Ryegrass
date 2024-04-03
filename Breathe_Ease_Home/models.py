b6e98c6887d549103963c1216984b1e94e2fd7ef
flashbird233 <moqianchen9811@gmail.com>
2024-04-03 23:20:34 +1100

    Renamed some error names in table ryegrass

--- Breathe_Ease_Home/models.py
+++ Breathe_Ease_Home/models.py
@@ -25,35 +25,35 @@ class Location(models.Model):
 
 
 # pyegrass record model
-class Pyegrass(models.Model):
+class Ryegrass(models.Model):
     # id
-    pye_id = models.IntegerField(primary_key=True, auto_created=True, null=False)
+    rye_id = models.IntegerField(primary_key=True, auto_created=True, null=False)
     # latitude
-    pye_lat = models.FloatField(max_length=10, default=None)
+    rye_lat = models.FloatField(max_length=10, default=None)
     # longitude
-    pye_lon = models.FloatField(max_length=10, default=None)
+    rye_lon = models.FloatField(max_length=10, default=None)
     # datetime
-    pye_datetime = models.DateTimeField(max_length=50, default=None, null=True)
+    rye_datetime = models.DateTimeField(max_length=50, default=None, null=True)
     # year
-    pye_year = models.IntegerField(default=None, null=True,
+    rye_year = models.IntegerField(default=None, null=True,
                                    validators=[MinValueValidator(1000), MaxValueValidator(2021)])
     # month
-    pye_month = models.IntegerField(default=None, null=True,
+    rye_month = models.IntegerField(default=None, null=True,
                                     validators=[MinValueValidator(1), MaxValueValidator(12)])
     # day
-    pye_day = models.IntegerField(default=None, null=True,
+    rye_day = models.IntegerField(default=None, null=True,
                                   validators=[MinValueValidator(1), MaxValueValidator(31)])
     # rye_record_by
-    pye_record_by = models.CharField(max_length=200, default=None, null=True)
+    rye_record_by = models.CharField(max_length=200, default=None, null=True)
     # rye_country
-    pye_country = models.CharField(max_length=50, default=None, null=True)
+    rye_country = models.CharField(max_length=50, default=None, null=True)
     # rye_country_code
-    pye_country_code = models.CharField(max_length=10, default=None, null=True)
+    rye_country_code = models.CharField(max_length=10, default=None, null=True)
     # rye_scientific_name
-    pye_scientific_name = models.CharField(max_length=100, default=None, null=True)
+    rye_scientific_name = models.CharField(max_length=100, default=None, null=True)
     # rye_vernacular_name
-    pye_vernacular_name = models.CharField(max_length=100, default=None, null=True)
+    rye_vernacular_name = models.CharField(max_length=100, default=None, null=True)
 
     # return pyegrass record
     def __str__(self):
-        return self.pye_lat, ", ", self.pye_lon, ", ", self.pye_datetime
+        return self.rye_lat, ", ", self.rye_lon, ", ", self.rye_datetime
