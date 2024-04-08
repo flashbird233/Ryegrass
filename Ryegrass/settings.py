efbeba4b1fd4ac9cfca75050b799ce63b1be8371
chee0031 <chee0031@student.monash.edu>
2024-04-08 19:57:27 +1000

    Merge remote-tracking branch 'origin/master'

--- Ryegrass/settings.py
+++ Ryegrass/settings.py
@@ -46,6 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ryegrass.azurewebsites.net",
 ]
 
 ROOT_URLCONF = 'Ryegrass.urls'
