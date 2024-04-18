b60e4ebf61da7651ac92617362525e1ad81f7182
flashbird233 <moqianchen9811@gmail.com>
2024-04-18 22:14:10 +1000

    Delete some useless codes in admin.py

--- Breathe_Ease_Home/admin.py
+++ Breathe_Ease_Home/admin.py
@@ -1,23 +1,9 @@
 from django.contrib import admin
-from django.urls import reverse
-from django.utils.html import format_html
 
 from .models import Location, Ryegrass, Symptom, Recommend, SymptomRecommends, SymptomStatistics
 
-
-class RyegrassAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'update_button')
-
-    def update_button(self, obj):
-        return format_html('<button type="button" onclick="location.href=\'{}\'">Update</button>',
-                           reverse('update_ryegrass', args=[obj.pk]))
-
-    update_button.short_description = 'Update Button'
-    update_button.allow_tags = True
-
-
 admin.site.register(Location)
-admin.site.register(Ryegrass, RyegrassAdmin)
+admin.site.register(Ryegrass)
 admin.site.register(Symptom)
 admin.site.register(Recommend)
 admin.site.register(SymptomRecommends)
