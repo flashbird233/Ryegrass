2490aaf8ce1c837a142ace5c93ab99abb83c4031
flashbird233 <moqianchen9811@gmail.com>
2024-04-24 17:21:23 +1000

    Recleaned the ryegrass records data, because the geo check for each searching is time consuming fo django beckend.

--- Breathe_Ease_Home/tests.py
+++ Breathe_Ease_Home/tests.py
@@ -1,2 +1,11 @@
-# Create your tests here.
-# Try the Deployment
+# Remove the records of ryegrass from the database
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ryegrass.settings')
+django.setup()
+
+from Breathe_Ease_Home.models import Ryegrass
+
+Ryegrass.objects.all().delete()
